f909fe6cd4b24799324ebf0e2e5eca37803c9004
Diivvuu <divyanshubawa9@gmail.com>
2024-09-24 02:59:29 +0530

    workspaces schema added, adding workspace in ui

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,8 +1,15 @@
-import { defineSchema } from "convex/server";
+import { v } from "convex/values";
+import { defineSchema, defineTable } from "convex/server";
+
 import { authTables } from "@convex-dev/auth/server";
 
 const schema = defineSchema({
   ...authTables,
+  workspaces: defineTable({
+    name: v.string(),
+    userId: v.id("users"),
+    joinCode: v.string(),
+  }),
   // Your other tables...
 });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,15 @@
 // import { Button } from "@/components/ui/button";
 import { useAuthActions } from "@convex-dev/auth/react";
 import { UserButton } from "@/features/auth/components/user-button";
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+import { useMemo } from "react";
 export default function Home() {
   const { signOut } = useAuthActions();
+  const { data, isLoading } = useGetWorkspaces();
+  const workspaceId = useMemo(() => data?.[0]?._id, [data]);
   return (
     <div className="min-h-screen flex items-center justify-center">
-      You are logged in user
+      {/* You are logged in user */}
       <UserButton />
     </div>
   );
