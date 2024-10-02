c9eb207dda0cd251293c065bc25e460c01a80636
Diivvuu <divyanshubawa9@gmail.com>
2024-10-03 03:59:55 +0530

    hook for update update workspace, new dialog for confirmation, functions of preferences working now

--- src/features/workspaces/api/use-remove-workspace.ts
+++ src/features/workspaces/api/use-remove-workspace.ts
@@ -1,35 +1,30 @@
-import { useMutation } from "convex/react";
-
-import { api } from "../../../../convex/_generated/api";
 import { useCallback, useMemo, useState } from "react";
 import { Id } from "../../../../convex/_generated/dataModel";
+import { useMutation } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
 
-type RequestType = { name: string };
+type RequestType = { id: Id<"workspaces"> };
 type ResponseType = Id<"workspaces"> | null;
+
 type Options = {
   onSuccess?: (data: ResponseType) => void;
   onError?: (error: Error) => void;
-  onSeettled?: () => void;
+  onSettled?: () => void;
   throwError?: boolean;
 };
 
-export const useCreateWorkspace = () => {
+export const useRemoveWorkspace = () => {
   const [data, setData] = useState<ResponseType>(null);
   const [error, setError] = useState<Error | null>(null);
-  const [status, setStatus] = useState
-    <"success" | "error" | "settled" | "pending" | null>(null
-  );
-
-  // const [isPending, setIsPending] = useState(false);
-  // const [isSuccess, setIsSuccess] = useState(false);
-  // const [isError, setIsError] = useState(false);
-  // const [isSettled, setIsSettled] = useState(false);
+  const [status, setStatus] = useState<
+    "success" | "error" | "settled" | "pending" | null
+  >(null);
   const isPending = useMemo(() => status === "pending", [status]);
   const isSuccess = useMemo(() => status === "success", [status]);
   const isError = useMemo(() => status === "error", [status]);
   const isSettled = useMemo(() => status === "settled", [status]);
 
-  const mutation = useMutation(api.workspaces.create);
+  const mutation = useMutation(api.workspaces.remove);
 
   const mutate = useCallback(
     async (values: RequestType, options?: Options) => {
@@ -50,7 +45,7 @@ export const useCreateWorkspace = () => {
         }
       } finally {
         setStatus("settled");
-        options?.onSeettled?.();
+        options?.onSettled?.();
       }
     },
     [mutation]
--- src/features/workspaces/api/use-update-workspace.ts
+++ src/features/workspaces/api/use-update-workspace.ts
@@ -4,32 +4,28 @@ import { api } from "../../../../convex/_generated/api";
 import { useCallback, useMemo, useState } from "react";
 import { Id } from "../../../../convex/_generated/dataModel";
 
-type RequestType = { name: string };
+type RequestType = { id: Id<"workspaces">; name: string };
 type ResponseType = Id<"workspaces"> | null;
 type Options = {
   onSuccess?: (data: ResponseType) => void;
   onError?: (error: Error) => void;
-  onSeettled?: () => void;
+  onSettled?: () => void;
   throwError?: boolean;
 };
 
-export const useCreateWorkspace = () => {
+export const useUpdateWorkspace = () => {
   const [data, setData] = useState<ResponseType>(null);
   const [error, setError] = useState<Error | null>(null);
-  const [status, setStatus] = useState
-    <"success" | "error" | "settled" | "pending" | null>(null
-  );
+  const [status, setStatus] = useState<
+    "success" | "error" | "settled" | "pending" | null
+  >(null);
 
-  // const [isPending, setIsPending] = useState(false);
-  // const [isSuccess, setIsSuccess] = useState(false);
-  // const [isError, setIsError] = useState(false);
-  // const [isSettled, setIsSettled] = useState(false);
   const isPending = useMemo(() => status === "pending", [status]);
   const isSuccess = useMemo(() => status === "success", [status]);
   const isError = useMemo(() => status === "error", [status]);
   const isSettled = useMemo(() => status === "settled", [status]);
 
-  const mutation = useMutation(api.workspaces.create);
+  const mutation = useMutation(api.workspaces.update);
 
   const mutate = useCallback(
     async (values: RequestType, options?: Options) => {
@@ -50,7 +46,7 @@ export const useCreateWorkspace = () => {
         }
       } finally {
         setStatus("settled");
-        options?.onSeettled?.();
+        options?.onSettled?.();
       }
     },
     [mutation]
