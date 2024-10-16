a8a4807fa107ae63eb93f85b8ee256c63e45ea56
Diivvuu <divyanshubawa9@gmail.com>
2024-10-17 05:15:39 +0530

    leftovers cleared

--- src/app/workspace/[workspaceId]/member/[memberId]/conversation.tsx
+++ src/app/workspace/[workspaceId]/member/[memberId]/conversation.tsx
@@ -6,6 +6,7 @@ import { Loader } from "lucide-react";
 import { Header } from "./header";
 import { ChatInput } from "./chat-input";
 import { MessageList } from "@/components/message-list";
+import { usePanel } from "@/hooks/use-panel";
 
 interface ConversationProps {
   id: Id<"conversations">;
@@ -13,6 +14,8 @@ interface ConversationProps {
 
 export const Conversations = ({ id }: ConversationProps) => {
   const memberId = useMemberId();
+
+  const { onOpenProfile, onClose } = usePanel();
 
   const { data: member, isLoading: memberLoading } = useGetMember({
     id: memberId,
@@ -34,7 +37,9 @@ export const Conversations = ({ id }: ConversationProps) => {
       <Header
         memberName={member?.user.name}
         memberImage={member?.user.image}
-        onClick={() => {}}
+        onClick={() => {
+          onOpenProfile(memberId);
+        }}
       />
       <MessageList
         data={results}
