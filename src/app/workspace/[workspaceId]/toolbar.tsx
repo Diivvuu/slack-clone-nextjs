8ad6465926f2d7d33259006f2b29a721a1cba73b
Diivvuu <divyanshubawa9@gmail.com>
2024-10-17 05:47:40 +0530

    fix build error (keys in search toolbar)

--- src/app/workspace/[workspaceId]/toolbar.tsx
+++ src/app/workspace/[workspaceId]/toolbar.tsx
@@ -51,7 +51,10 @@ const Toolbar = () => {
           <CommandList>
             <CommandGroup heading="Channels">
               {channels?.map((channel) => (
-                <CommandItem onSelect={() => onChannelClick(channel._id)}>
+                <CommandItem
+                  key={channel._id}
+                  onSelect={() => onChannelClick(channel._id)}
+                >
                   {channel.name}
                 </CommandItem>
               ))}
@@ -60,7 +63,10 @@ const Toolbar = () => {
           <CommandSeparator />
           <CommandGroup heading="Members">
             {members?.map((member) => (
-              <CommandItem onSelect={() => onMemberClick(member._id)}>
+              <CommandItem
+                key={member._id}
+                onSelect={() => onMemberClick(member._id)}
+              >
                 {member.user.name}
               </CommandItem>
             ))}
